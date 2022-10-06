24cdbb2cd3a0e09dc197262e04bb826a76011bd1
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-10-06 10:03:04 +0900

    Add CLI test

---
+++ test/cli_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+require "open3"
+
+using Easytest::DSL
+
+def run(cmd)
+  stdout, stderr, status = Open3.capture3(cmd)
+  [stdout, stderr, status.exitstatus]
+end
+
+test "show help" do
+  stdout, stderr, status = run("easytest --help")
+  expect(stdout).to_eq <<-MSG
+USAGE
+  easytest [options] [...<file, directory, or pattern>]
+
+OPTIONS
+  --help      Show help
+  --version   Show version
+MSG
+  expect(stderr).to_eq ""
+  expect(status).to_eq 0
+end
+
+test "show version" do
+  stdout, stderr, status = run("easytest --version")
+  expect(stdout).to_eq "easytest #{Easytest::VERSION}\n"
+  expect(stderr).to_eq ""
+  expect(status).to_eq 0
+end
+
+test "invalid option" do
+  stdout, stderr, status = run("easytest --foo")
+  expect(stdout).to_eq ""
+  expect(stderr).to_eq "invalid option: --foo\n"
+  expect(status).to_eq 2
+end
